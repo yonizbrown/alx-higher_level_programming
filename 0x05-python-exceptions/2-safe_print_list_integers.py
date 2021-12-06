09c965fbc728431c39cecfea0fd408b39b6f6c58
Jello/Reji <tasha984288@gmail.com>
2021-12-06 10:53:06 -0800

    new commit

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-count = 0
-
-for i in range(x):
-try:
-print("{:d}".format(my_list[i]), end='')
-except TypeError:
-pass
-except ValueError:
-pass
-else:
-count += 1
-
-print()
-return (count)
+    try:
+        count = 0
+        for i in range(x):
+            if isinstance(my_list[i], int):
+                count += 1
+                print("{:d}".format(my_list[i]), end="")
+    except TypeError as err:
+        print(err)
+    else:
+        print("")
+        return count
--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    new_list = []
+    store = []
+    switch = 0
     for i in range(list_length):
         try:
-            result = my_list_1[i] / my_list_2[i]
+            x = my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
             print("division by 0")
-            result = 0
+            switch = 1
         except TypeError:
             print("wrong type")
-            result = 0
+            switch = 1
         except IndexError:
             print("out of range")
-            result = 0
+            switch = 1
         finally:
-            new_list.append(result)
-
-    return (new_list)
+            if switch:
+                switch = 0
+                store.append(0)
+            else:
+                store.append(x)
+    return store
