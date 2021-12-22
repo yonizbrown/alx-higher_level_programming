2761d0d324f74fd9b4068e600d424bf6e2efaa10
Jello/Reji <tasha984288@gmail.com>
2021-12-22 06:11:48 -0800

    Project python-inheritance

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,30 +1,41 @@
 #!/usr/bin/python3
+"""
+Module 12-pascal_triangle
+Contains function that returns int lists of pascal triangle of any given size
+"""
+
+
 def pascal_triangle(n):
-    """ Function that returns the pascal triangle
-
-    Args:
-        n: number of lines
-
-    Returns:
-        matrix: a matrix with the pascal triangle
-
     """
+    Return:
+        empty list [] if n <= 0
+        if n is 7, we should expect:
+            [1]
+            [1, 1]
+            [1, 2, 1]
+            [1, 3, 3, 1]
+            [1, 4, 6, 4, 1]
+            [1, 5, 10, 10, 5, 1]
+            [1, 6, 15, 20, 15, 6, 1]
+    """
+    if n <= 0:
+        return []
+    if n == 1:
+        return [[1]]
 
-    matrix = []
-    prev = []
+    triangle = [[1]]
+    for rows in range(n-1):
+        triangle.append([a+b for a, b
+                         in zip([0] + triangle[-1], triangle[-1] + [0])])
+    return triangle
 
-    for i in range(n):
-        res_list = []
-        p1 = -1
-        p2 = 0
-        for j in range(len(prev) + 1):
-            if p1 == -1 or p2 == len(prev):
-                res_list += [1]
-            else:
-                res_list += [prev[p1] + prev[p2]]
-            p1 += 1
-            p2 += 1
-        matrix.append(res_list)
-        prev = res_list[:]
-
-    return matrix
+# C implementation of the last Pythonic loop
+#
+#    triangle = [[1]]
+#    for rows in range(n-1):
+#        l = [1]
+#        for i in range(rows):
+#            l.append(triangle[-1][i] + triangle[-1][i+1])
+#        l.append(1)
+#        triangle.append(l)
+#    return triangle
--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,19 +1,27 @@
 #!/usr/bin/python3
-""" Module that adds all arguments to a Python list, and then
-save them to a file
 """
-import sys
-import os.path
+Module 7-add_item
+Contains function that adds and saves to Python obj to JSON file; loads objects
+# run with ./7-add_item.py
+#
+# cat add_item.json ; echo ""
+# expect output: []
+#
+# ./7-add_item.py some random args
+# cat add_item.json ; echo ""
+# expect output: ["some", "random", "args"]
+"""
 
 
-save_file = __import__('7-save_to_json_file').save_to_json_file
-load_file = __import__('8-load_from_json_file').load_from_json_file
+from sys import argv
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-my_list = []
-if os.path.exists("add_item.json"):
-my_list = load_file("add_item.json")
+filename = "add_item.json"
 
-for arg in sys.argv[1:]:
-my_list.append(arg)
+try:
+    existing_content = load_from_json_file(filename)
+except FileNotFoundError:
+    existing_content = []
 
-save_file(my_list, "add_item.json")
+save_to_json_file(existing_content + argv[1:], filename)
