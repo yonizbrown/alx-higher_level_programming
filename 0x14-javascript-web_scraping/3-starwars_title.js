5de5e36314e382ce43dd5e6e50c40c92b92c1522
yonizbrown <yoniz0915@gmail.com>
2022-04-13 12:02:59 +0300

    new project

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,9 +1,16 @@
 #!/usr/bin/node
+
+let id = process.argv[2];
+let url = 'http://swapi.co/api/films/' + id;
 const request = require('request');
-const id = process.argv[2];
-request('https://swapi-api.hbtn.io/api/films/:id' + id + '/', function (error, response, body) {
-  if (error == null) {
-    const json = JSON.parse(body);
-    console.log(json.title);
-  } 
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    body = JSON.parse(body);
+    console.log(body['title']);
+  } else {
+    console.log('Erorr Code:' + response.statusCode);
+  }
 });
